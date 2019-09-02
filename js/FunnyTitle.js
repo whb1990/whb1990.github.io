d1ca6243789ef12d351338e88dd0306d543f1e54
wanghongbo <270028806@qq.com>
2019-09-02 15:17:25 +0800

    Site updated: 2019-09-02 15:17:04

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -3,12 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/images/warning_32x32.ico");
          document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
          clearTimeout(titleTime);
      }
      else {
-         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         $('[rel="icon"]').attr('href', "/images/favicon-32x32.ico");
          document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
