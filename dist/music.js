2737408f6445342e79618bf47a5539cea7fdc3da
wanghongbo <270028806@qq.com>
2019-09-10 17:58:24 +0800

    Site updated: 2019-09-10 17:58:16

--- dist/music.js
+++ dist/music.js
@@ -13,9 +13,9 @@ const ap = new APlayer({
     mutex: false,                                    //互斥，阻止多个播放器同时播放，当前播放器播放时暂停其他播放器
 	audio: [                                        //音频信息
         {
-            name: '응급실',                               //音频名称
-            artist: '정승환',                       //音频艺术家
-            url: 'https://avos-cloud-jej3f06miele.s3.amazonaws.com/c66e52252291d2fa8947/%E8%B1%AA%E6%9D%B0%E6%98%A5%E9%A6%99%20-%20%E6%9C%89%E5%BF%85%E8%A6%81%E4%B8%8D%E5%A5%BD%E6%84%8F%E6%80%9D%E5%90%97.mp3',                         //音频链接
+            name: '미안해야 하는거니 (有必要不好意思吗)',                               //音频名称
+            artist: '애즈 원',                       //音频艺术家
+            url: 'http://mobileoc.music.tc.qq.com/M500000oW4mX3AqDXY.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&fromtag=53&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD',                         //音频链接
             cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000003x5xa32jA2DE.jpg',                     //音频封面
             lrc: 'http://lab.liumingye.cn/api/lrc/get/type/qq/mid/000JoF1Q0GWBM0/download/1/name/%EC%9D%91%EA%B8%89%EC%8B%A4%20(%E6%80%A5%E6%95%91%E5%AE%A4)%20-%20%EC%A0%95%EC%8A%B9%ED%99%98.lrc',                         //歌词
             theme: '#ebd0c2'                           //切换到此音频时的主题色，比上面的 theme 优先级高
@@ -27,15 +27,15 @@ const ap = new APlayer({
             cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000000fvwBw4aRYLv.jpg?max_age=2592000',
             lrc: 'http://lab.liumingye.cn/api/lrc/get/type/qq/mid/000dtSva1Mzebi/download/1/name/%ED%96%89%EB%B3%B5%ED%95%98%EA%B8%B8%20%EB%B0%94%EB%9E%98%20(%E5%B8%8C%E6%9C%9B%E4%BD%A0%E5%B9%B8%E7%A6%8F)%20-%20%EC%9E%84%ED%98%95%EC%A3%BC.lrc',
             theme: '#46718b'
-        }
+        },
 		{
-            name: 'Something just like this',
-            artist: 'Alex Goot _ Madilyn Bailey',
-            url: 'https://avos-cloud-jej3f06miele.s3.amazonaws.com/efbd2e1faef47bebf5c3/Alex%20Goot%20_%20Madilyn%20Bailey%20-%20Something%20Just%20Like%20This.mp3',
-            cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000001fyOld32C1nv.jpg',
-            lrc: 'http://lab.liumingye.cn/api/lrc/get/type/qq/mid/004JaCzc1KhTrU/download/1/name/Something%20Just%20Like%20This%20-%20The%20Chainsmokers%2CColdplay.lrc',
+            name: '응급실 (急救室)',
+            artist: '정승환',
+            url: 'http://mobileoc.music.tc.qq.com/M500000AxSPm1dUAlM.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&fromtag=53&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD',
+            cover: 'http://y.gtimg.cn/music/photo_new/T002R800x800M000003x5xa32jA2DE.jpg',
+            lrc: 'http://lab.liumingye.cn/api/lrc/get/type/qq/mid/000JoF1Q0GWBM0/download/1/name/%EC%9D%91%EA%B8%89%EC%8B%A4%20(%E6%80%A5%E6%95%91%E5%AE%A4)%20-%20%EC%A0%95%EC%8A%B9%ED%99%98.lrc',
             theme: '#46718b'
-        }
+        },
 		{
             name: 'MyLove',
             artist: 'WestLife',
